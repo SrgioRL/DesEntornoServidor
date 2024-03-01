b3787e27b61a8c3ad28ac7df457fd7aaf634f24e
SergioRL <sergiorl1989@gmail.com>
2024-03-01 19:11:13 +0100

    First commit

---
+++ Z_ExamenServidor/src/main/java/examen/ZExamenServidorApplication.java
@@ -0,0 +1,13 @@
+package examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZExamenServidorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZExamenServidorApplication.class, args);
+	}
+
+}
