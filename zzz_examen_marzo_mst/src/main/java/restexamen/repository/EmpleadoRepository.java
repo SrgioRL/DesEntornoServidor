bfabd86e275b6ec9f294685964ec497e57f481f1
SergioRL <sergiorl1989@gmail.com>
2024-03-01 18:42:25 +0100

    Deleted unused project

--- zzz_examen_marzo_mst/src/main/java/restexamen/repository/EmpleadoRepository.java
+++
@@ -1,9 +0,0 @@
-package restexamen.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import restexamen.modelo.entities.Empleado;
-
-public interface EmpleadoRepository extends JpaRepository<Empleado, Integer> {
-
-}
